30fb1dabd5174e89e30741ed08699e379d409978
sisi0318 <gamemode12345@163.com>
2025-06-11 19:12:47 +0800

    cors一顿关

--- api.py
+++ api.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI, Request, Form
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 
 import module.wopan as Wopan
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # 允许所有源
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 # 处理 GET 参数
 @app.get("/api/wopan/sms")
